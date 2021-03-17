7a320b41b3255533179fe87be09e83e4e91ed587
James Morales <jamesmoralesmoreno@gmail.com>
2021-03-16 21:07:20 -0500

    kernel

---
+++ Kernel/Kernel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Kernel
+{
+    class Kernel
+    {
+
+        /*
+         * File sistem port: 8082 
+         * GUI port: 8081
+         * Kernel port: 8080
+         * 
+         */
+        static void Main(string[] args)
+        {
+            Kernel core = new Kernel();
+            Comunication comunicationSet = new Comunication();
+            Messages messages = new Messages(core, comunicationSet);
+
+            comunicationSet.setterMessages(messages);
+
+            Thread listener = new Thread(() => comunicationSet.StartListening(8080));
+            listener.Start();
+
+            comunicationSet.sendMessage("{cmd:stop, src:APP1, dst:kernel, msg”Err->División por 0”}", 8080);
+
+        }
+
+
+        private void initilizationCore()
+        {
+            //TODO:
+        }
+
+        public void stopKernel()
+        {
+            //TODO: 
+        }
+
+
+    }
+}
---
+++ Kernel/Messages.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kernel
+{
+    /*
+         * File sistem port: 8082 
+         * GUI port: 8081
+         * Kernel port: 8080
+         * 
+         */
+    class Messages
+    {
+        private Comunication comunication;
+        private Kernel core;
+
+        public Messages(Kernel core, Comunication comunication)
+        {
+            this.comunication = comunication;
+            this.core = core;
+
+        }
+
+
+        public void Actions(string msg)
+        {
+
+            string[] msgClean = msg.Replace("<EOF>", "").Replace("{", "").Replace("}", "").Split(',');
+
+            if (msgClean.Length > 2)
+            {
+                longMessage(msgClean, msg);
+            }
+            else
+            {
+                shortMessage(msgClean);
+            }
+
+        }
+
+        private void longMessage(string[] msgClean, string rawMsg)
+        {
+            switch (msgClean[2].Split(':')[1])
+            {
+                case "GUI":
+                    comunication.sendMessage(rawMsg, 8081);
+                    break;
+                case "GestorArc":
+                    comunication.sendMessage(rawMsg, 8082);
+                    break;
+                case "kernel":
+                    //comunication.sendMessage(rawMsg, 8082);
+                    core.stopKernel();
+                    break;
+                case "APP":
+                    comunication.sendMessage(rawMsg, 8083);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void shortMessage(string[] msgClean)
+        {
+            switch (msgClean[1].Replace("\"", "").Split(':')[1])
+            {
+                case "OK":
+                    break;
+                case "0":
+
+                    break;
+                case "Err":
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+
+
+    }
+}
